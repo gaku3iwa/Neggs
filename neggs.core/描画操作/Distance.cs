317c9bc303e091af304a632ad7f91d518c489bc9
ponkotu <gaku3@mac.com>
2017-07-21 18:33:06 +0900

    wpf 依存性分離

--- neggs.core/描画操作/Distance.cs
+++ neggs.core/描画操作/Distance.cs
@@ -11,18 +11,6 @@ namespace neggs.core
 		/// <param name="p1"></param>
 		/// <param name="p2"></param>
 		/// <returns></returns>
-		public static double Distance(this System.Windows.Input.StylusPoint p1, System.Windows.Input.StylusPoint p2)
-		{
-			return Math.Sqrt(p1.X.Distance(p2.X) * p1.X.Distance(p2.X)
-						   + p1.Y.Distance(p2.Y) * p1.Y.Distance(p2.Y));
-		}
-
-		/// <summary>
-		/// ２点間の距離を求める
-		/// </summary>
-		/// <param name="p1"></param>
-		/// <param name="p2"></param>
-		/// <returns></returns>
 		public static double Distance(this System.Drawing.Point p1, System.Drawing.Point p2)
 		{
 			return Math.Sqrt(p1.X.Distance(p2.X) * p1.X.Distance(p2.X)
---
+++ neggs.wpf.core/描画操作/Distance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace neggs.core
+{
+	public static partial class Extensions
+	{
+
+		/// <summary>
+		/// ２点間の距離を求める
+		/// </summary>
+		/// <param name="p1"></param>
+		/// <param name="p2"></param>
+		/// <returns></returns>
+		public static double Distance(this System.Windows.Input.StylusPoint p1, System.Windows.Input.StylusPoint p2)
+		{
+			return Math.Sqrt(p1.X.Distance(p2.X) * p1.X.Distance(p2.X)
+						   + p1.Y.Distance(p2.Y) * p1.Y.Distance(p2.Y));
+		}
+
+	}
+}
